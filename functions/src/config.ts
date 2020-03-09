b695565c6f45b690f0f81feaaddf458075aac874
Abdealikw <abdealikw@gmail.com>
2020-03-09 15:06:10 +0530

    changing structure of project

---
+++ functions/src/config.ts
@@ -0,0 +1,7 @@
+const serviceAccount = require("../classroom-door-firebase-adminsdk-6perx-dbae20c4c1.json");
+const Config = {
+	serviceAccount,
+	databaseURL: "https://classroom-door.firebaseio.com"
+};
+
+export default Config;
--- functions/src/students/index.ts
+++ functions/src/students/index.ts
@@ -1,15 +1,19 @@
-import { studentCollection, userCollection } from "..";
+import { studentCollection, userCollection } from "../db";
 
 export const getAllStudents = async () => {
-    const studentPreferencesSnapshot = await studentCollection.get();
+	const studentPreferencesSnapshot = await studentCollection.get();
 
-    const allStudentsData = studentPreferencesSnapshot.docs.map(studentDoc => {
-        const studentDetails = studentDoc.data();
-        const userID = studentDetails.userId;
-        return userCollection
-            .doc(userID)
-            .get()
-            .then(data => ({ ...studentDetails, ...data.data() }));
-    });
-    return Promise.all(allStudentsData);
+	const allStudentsData = studentPreferencesSnapshot.docs.map(studentDoc => {
+		const studentDetails = studentDoc.data();
+		const userID = studentDetails.userId;
+		return userCollection
+			.doc(userID)
+			.get()
+			.then(data => ({ ...studentDetails, ...data.data() }));
+	});
+	return Promise.all(allStudentsData);
+};
+
+export const markAsFavourite = (req: any, res: any) => {
+	res.send("hello world");
 };
