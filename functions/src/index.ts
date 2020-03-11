aa55b31b15b7c14cb27e8cf77bdc4a2cb3c13cee
Abdealikw <abdealikw@gmail.com>
2020-03-11 13:41:16 +0530

    Completed working on mark as favorite API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,101 +4,106 @@ import { getTeacherDataWithFilters } from "./teachers";
 const serviceAccount = require("../classroom-door-firebase-adminsdk-6perx-dbae20c4c1.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://classroom-door.firebaseio.com"
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://classroom-door.firebaseio.com"
 });
 // import { } from './subjects';
 import { getAllUsers, getUsersById } from "./users";
-import { getAllStudents } from "./students";
-import { updateTeacherPreference, updateUserProfileDetails } from "./update-user-data";
+import * as Students from "./students";
+import {
+	updateTeacherPreference,
+	updateUserProfileDetails
+} from "./update-user-data";
 
 export const firestoreDB = admin.firestore();
 export const userCollection = firestoreDB.collection("users");
 export const teacherCollection = firestoreDB.collection("teachersCopy");
 export const studentCollection = firestoreDB.collection("students");
-export const userMetaDataCollection = firestoreDB.collectionGroup('userMeta');
-
-
+export const userMetaDataCollection = firestoreDB.collectionGroup("userMeta");
 
 // Get Users
-export const users = functions.https.onCall((request: { userIds?: string[] }) => {
-  console.log("User endpoint Request::", request);
-  const { userIds } = request;
-  // check if request is for some userIds
-  if (userIds && userIds.length > 0) {
-    return getUsersById(userIds)
-      .then(res => res)
-      .catch(err => new Error(err))
-  }
+export const users = functions.https.onCall(
+	(request: { userIds?: string[] }) => {
+		console.log("User endpoint Request::", request);
+		const { userIds } = request;
+		// check if request is for some userIds
+		if (userIds && userIds.length > 0) {
+			return getUsersById(userIds)
+				.then(res => res)
+				.catch(err => new Error(err));
+		}
 
-  return getAllUsers()
-    .then(res => res)
-    .catch(err => new Error(err))
-});
+		return getAllUsers()
+			.then(res => res)
+			.catch(err => new Error(err));
+	}
+);
 
 // Get Teachers
 export const teachers = functions.https.onCall((request: any) => {
-  console.log("Request Parameters::::", JSON.stringify(request));
-  return getTeacherDataWithFilters(request.filters)
-    .then(res => {
-      console.log("Teachers Data :::", res)
-      return res;
-    })
-    .catch(err => {
-      console.log("Found Error:::", err)
-      return new Error(err)
-    });
+	console.log("Request Parameters::::", JSON.stringify(request));
+	return getTeacherDataWithFilters(request.filters)
+		.then(res => {
+			console.log("Teachers Data :::", res);
+			return res;
+		})
+		.catch(err => {
+			console.log("Found Error:::", err);
+			return new Error(err);
+		});
 });
 
 export const getTeachersAPI = functions.https.onRequest(
-  async (request: any, response: any) => {
-    getTeacherDataWithFilters(request.body.filters).then(res => {
-      console.log("Data Final::", res)
-      response.status(200).json({
-        data: res,
-        message: "Query Success"
-      });
-    })
-      .catch(err => console.log("query Failed: ", err));
-  }
+	async (request: any, response: any) => {
+		getTeacherDataWithFilters(request.body.filters)
+			.then(res => {
+				console.log("Data Final::", res);
+				response.status(200).json({
+					data: res,
+					message: "Query Success"
+				});
+			})
+			.catch(err => console.log("query Failed: ", err));
+	}
 );
-
 
 // Get Students
 export const students = functions.https.onRequest((request: any) => {
-  return getAllStudents()
-    .then(res => res)
-    .catch(err => new Error(err))
-})
-
+	return Students.getAllStudents()
+		.then(res => res)
+		.catch(err => new Error(err));
+});
 
 // Update API Calls
-// User Details 
+// User Details
 export const fixTeacherPreferences = functions.https.onRequest(
-  (request: any, response: any) => {
-    updateTeacherPreference()
-      .then(res => {
-        response.status(200).json({
-          data: res,
-          message: "Updated Teacher List Success"
-        });
-      })
-      .catch(err =>
-        console.log("Found Error on updating Teacher  List: ", err)
-      );
-  }
+	(request: any, response: any) => {
+		updateTeacherPreference()
+			.then(res => {
+				response.status(200).json({
+					data: res,
+					message: "Updated Teacher List Success"
+				});
+			})
+			.catch(err =>
+				console.log("Found Error on updating Teacher  List: ", err)
+			);
+	}
 );
 
-export const updateUserData = functions.https.onRequest((request: any, response: any) => {
-  updateUserProfileDetails()
-    .then(res => {
-      response.status(200).json({
-        data: res,
-        message: "Updated user Data List Success"
-      });
-    })
-    .catch(err =>
-      console.log("Found Error on updating user Data List: ", err)
-    );
-})
+export const updateUserData = functions.https.onRequest(
+	(request: any, response: any) => {
+		updateUserProfileDetails()
+			.then(res => {
+				response.status(200).json({
+					data: res,
+					message: "Updated user Data List Success"
+				});
+			})
+			.catch(err =>
+				console.log("Found Error on updating user Data List: ", err)
+			);
+	}
+);
 
+export const toggleMarkAsFavourite = Students.toggleMarkAsFavourite;
--- functions/src/students/index.ts
+++ functions/src/students/index.ts
@@ -1,15 +1,53 @@
+import * as functions from "firebase-functions";
 import { studentCollection, userCollection } from "..";
 
 export const getAllStudents = async () => {
-    const studentPreferencesSnapshot = await studentCollection.get();
+	const studentPreferencesSnapshot = await studentCollection.get();
 
-    const allStudentsData = studentPreferencesSnapshot.docs.map(studentDoc => {
-        const studentDetails = studentDoc.data();
-        const userID = studentDetails.userId;
-        return userCollection
-            .doc(userID)
-            .get()
-            .then(data => ({ ...studentDetails, ...data.data() }));
-    });
-    return Promise.all(allStudentsData);
+	const allStudentsData = studentPreferencesSnapshot.docs.map(studentDoc => {
+		const studentDetails = studentDoc.data();
+		const userID = studentDetails.userId;
+		return userCollection
+			.doc(userID)
+			.get()
+			.then(data => ({ ...studentDetails, ...data.data() }));
+	});
+	return Promise.all(allStudentsData);
 };
+
+export const toggleMarkAsFavourite = functions.https.onRequest(
+	async (req: any, res: any) => {
+		const { studentId, teacherId } = req.body;
+		const studentDoc = studentCollection.doc(studentId);
+		const student = await studentDoc.get().then((doc: any) => doc.data());
+
+		student.userMeta =
+			student.userMeta === undefined
+				? {
+						favorites: [teacherId]
+				  }
+				: {
+						...student.userMeta,
+						favorites: student.userMeta.favorites.filter(
+							(f: String) => f !== teacherId
+						)
+				  };
+
+		studentDoc
+			.update({
+				userMeta: student.userMeta
+			})
+			.then(() =>
+				res.json({
+					success: true,
+					message: "Mark as favorite status changed!"
+				})
+			)
+			.catch(err =>
+				res.json({
+					success: false,
+					message: err
+				})
+			);
+	}
+);
