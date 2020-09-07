f343e1f2c9240798de6d2155650277f807abe044
Shardul Singh <shardul@particle41.com>
2020-09-07 13:01:44 +0530

    user-meta trigger fix

---
+++ functions/src/users/triggers.ts
@@ -0,0 +1,27 @@
+import * as functions from "firebase-functions"
+import { firestoreCollectionKeys } from "../db/enum"
+import { userMetaCollection } from "../db"
+
+/** USER TRIGGERS **/
+
+// New User Creation Trigger
+export const newUserCreationTrigger = functions.firestore
+    .document(`${firestoreCollectionKeys.USERS}/{userId}`)
+    .onCreate((snapshot, _) => {
+        userMetaCollection
+            .doc(snapshot.id)
+            .set({
+                avgRating: "0",
+                favorites: [],
+                minutesTutoring: 0,
+                reviews: [],
+                sessionsCompleted: 0,
+                totalRatingsPosted: 0,
+                totalStars: 0,
+                userId: snapshot.id,
+            })
+            .catch((err) => {
+                console.log("Error Creating User-Meta Document for: ", snapshot.id)
+                console.log("Error Details: ", err)
+            })
+    })
