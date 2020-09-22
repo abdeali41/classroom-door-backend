0cdd5edf99724b4afc3165be9097962867dea562
Abdealikw <abdealikw@gmail.com>
2020-09-22 12:35:33 +0530

    added. env for stripe

--- functions/src/libs/Stripe.ts
+++ functions/src/libs/Stripe.ts
@@ -1,11 +1,9 @@
 import Stripe from "stripe";
-
-const stripe = new Stripe(
-	"sk_test_51H33lzEidh2gujSuiBX3hbXp8PgHfAdv83rGfkdXYIEjXsgp4ePn01sD2OoxK80b2v5sRR4mBkBXnG65lM6Av3RJ001VQ2YpYR",
-	{
-		apiVersion: "2020-08-27",
-		typescript: true,
-	}
-);
+const stripeKey: any =
+	process.env.STRIPE_PROD_KEY || process.env.STRIPE_DEV_KEY;
+const stripe = new Stripe(stripeKey, {
+	apiVersion: "2020-08-27",
+	typescript: true,
+});
 
 export default stripe;
