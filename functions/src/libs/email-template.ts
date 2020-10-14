f98004d3df09e688c975e1857efbf1f511e60e32
Abdealikw <abdealikw@gmail.com>
2020-10-14 13:52:35 +0530

    removing green background from link

--- functions/src/libs/email-template.ts
+++ functions/src/libs/email-template.ts
@@ -13,7 +13,7 @@ export const sessionRequestedToTutor = ({
             <p><br></p>
             <p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;"><span style="font-size:12pt;font-family:Arial;color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">${studentName} has requested a session with you!&nbsp;</span></p>
             <p><br></p>
-            <p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;"><span style="font-size:12pt;font-family:Arial;color:#000000;background-color:#00ff00;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">${CLASSROOMDOOR_WEB_URL}</span></p>
+            <p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;"><a style="font-size:12pt;font-family:Arial;color:#000000;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">${CLASSROOMDOOR_WEB_URL}</a></p>
             <p><br></p>
             <p><br></p>
             <p dir="ltr" style="line-height:1.38;margin-top:0pt;margin-bottom:0pt;"><span style="font-size:12pt;font-family:Arial;color:#000000;background-color:transparent;font-weight:400;font-style:normal;font-variant:normal;text-decoration:none;vertical-align:baseline;white-space:pre;white-space:pre-wrap;">All the best,&nbsp;</span></p>
