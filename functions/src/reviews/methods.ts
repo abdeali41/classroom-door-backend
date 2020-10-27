c5bb1b7a6a02d0fc2ea6a79f9fe4f511c1c2c6e1
Shardul Singh <shardul@particle41.com>
2020-10-27 15:24:07 +0530

    Code Prettify

--- functions/src/reviews/methods.ts
+++ functions/src/reviews/methods.ts
@@ -1,37 +1,43 @@
-import * as admin from "firebase-admin";
-import { epicboardSessionCollection, reviewsCollection, userMetaCollection } from "../db";
+import * as admin from "firebase-admin"
+import { epicboardSessionCollection, reviewsCollection, userMetaCollection } from "../db"
 
-const updatedAvergeRating : any = (
-    totalStarsBeforeUpdate:number, 
-    ratingCounterBeforeUpdate: number, 
-    newStars: number) => ((totalStarsBeforeUpdate+newStars)/(ratingCounterBeforeUpdate + 1)).toPrecision(2);
+const updatedAvergeRating: any = (
+    totalStarsBeforeUpdate: number,
+    ratingCounterBeforeUpdate: number,
+    newStars: number,
+) => ((totalStarsBeforeUpdate + newStars) / (ratingCounterBeforeUpdate + 1)).toPrecision(2)
 
 //Get All Review Documents from Reviews Collection
 export const getReviews = async (): Promise<any> => {
-	const reviewSnapshot = await reviewsCollection.get();
-	const reviews = reviewSnapshot.docs.map((reviewDoc) => reviewDoc.data());
-	return reviews;
-};
+    const reviewSnapshot = await reviewsCollection.get()
+    const reviews = reviewSnapshot.docs.map((reviewDoc) => reviewDoc.data())
+    return reviews
+}
 
 export const updateRatingsAndReviewInUserMeta = async (
-	params: updateRatingsAndReviewInUserMetaParams
+    params: updateRatingsAndReviewInUserMetaParams,
 ): Promise<void> => {
-	const { reviewId, reviewData } = params;
-	const { addresseeId } = reviewData;
-	const oldData = (await userMetaCollection.doc(addresseeId).get()).data()
-	const sessionData = (await epicboardSessionCollection.doc(reviewData.sessionId).get()).data()
-	try {await userMetaCollection.doc(addresseeId).update({
-			avgRating: updatedAvergeRating(oldData?.totalStars, oldData?.totalRatingsPosted, reviewData.rating),
-			reviews: admin.firestore.FieldValue.arrayUnion(reviewId),
-			totalStars: admin.firestore.FieldValue.increment(reviewData.rating),
-			totalRatingsPosted: admin.firestore.FieldValue.increment(1),
-			totalMinutesTutoring: admin.firestore.FieldValue.increment(sessionData?.sessionLength)
-		});
-		
-		await epicboardSessionCollection.doc(reviewData.sessionId).update({
-			reviewPosted: true
-		})
-	} catch (err) {
-		console.log("Error Updating Meta Trigger document:", err);
-	}
-};
+    const { reviewId, reviewData } = params
+    const { addresseeId } = reviewData
+    const oldData = (await userMetaCollection.doc(addresseeId).get()).data()
+    const sessionData = (await epicboardSessionCollection.doc(reviewData.sessionId).get()).data()
+    try {
+        await userMetaCollection.doc(addresseeId).update({
+            avgRating: updatedAvergeRating(
+                oldData?.totalStars,
+                oldData?.totalRatingsPosted,
+                reviewData.rating,
+            ),
+            reviews: admin.firestore.FieldValue.arrayUnion(reviewId),
+            totalStars: admin.firestore.FieldValue.increment(reviewData.rating),
+            totalRatingsPosted: admin.firestore.FieldValue.increment(1),
+            totalMinutesTutoring: admin.firestore.FieldValue.increment(sessionData?.sessionLength),
+        })
+
+        await epicboardSessionCollection.doc(reviewData.sessionId).update({
+            reviewPosted: true,
+        })
+    } catch (err) {
+        console.log("Error Updating Meta Trigger document:", err)
+    }
+}
